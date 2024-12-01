56630ca85118399934d0e8b01d033e0048db8ad6
Northory <northwind@tsar.freeqiye.com>
2024-12-01 18:16:13 +0800

    初始化代码

---
+++ BoreyAdSDK/Base/BoreyAd.h
@@ -0,0 +1,13 @@
+//
+//  BoreyAd.h
+//  BoreyAdSDK
+//
+//  Created by Buer on 2024/12/1.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger BoreyAdType) {
+    Splash = 0,
+    Express
+};
---
+++ BoreyAdSDK/BoreyAdSDK.h
@@ -0,0 +1,19 @@
+//
+//  BoreyAdSDK.h
+//  BoreyAdSDK
+//
+//  Created by Buer on 2024/12/1.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BoreyAdSDK.
+FOUNDATION_EXPORT double BoreyAdSDKVersionNumber;
+
+//! Project version string for BoreyAdSDK.
+FOUNDATION_EXPORT const unsigned char BoreyAdSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoreyAdSDK/PublicHeader.h>
+
+
+
